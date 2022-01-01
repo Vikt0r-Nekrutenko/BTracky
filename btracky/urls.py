487d6450b6d5e961e1b2963446463fb3847024b6
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-01 21:49:35 +0200

    003

--- btracky/urls.py
+++ btracky/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-import analysis
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', analysis.urls),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    path('', include('analysis.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
