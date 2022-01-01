572f8717c8270cf9b8b900f80e10aeeee34120ff
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-02 01:37:03 +0200

    006

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    earned = models.PositiveIntegerField(default=0)
+    hold = models.IntegerField(default=0)
+    difference = models.IntegerField(default=0)
