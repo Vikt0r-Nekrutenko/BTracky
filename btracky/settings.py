fa4ba11368781c4f1eefc437670e3b245f4a9dbd
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-03 16:44:46 +0200

    031

--- btracky/settings.py
+++ btracky/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-am9_(y^lq&o!*&o^qq4c2+_+4e=jk3f=%*c)d60^*$ldvtsk#2
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.0.105'
+    '192.168.0.105',
+    'localhost',
 ]
 
 
@@ -125,7 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, '../account/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
