b94ce136060566aecaea254e31234579e65e8768
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-04 23:06:06 +0200

    047

--- history/urls.py
+++ history/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -8,3 +10,5 @@ urlpatterns = [
     path('', views.history, name='history'),
     path(r'<part_id>', views.remove, name='remove'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
