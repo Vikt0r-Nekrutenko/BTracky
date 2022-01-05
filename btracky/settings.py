e7d216105e3e59f7a54d861e4f5159be8038f1d8
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-05 18:23:30 +0200

    073

--- btracky/settings.py
+++ btracky/settings.py
@@ -129,7 +129,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 
