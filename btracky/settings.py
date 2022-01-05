7da46cc07bc9d76a59e90064fc033915b937e6a2
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-05 18:21:57 +0200

    071

--- btracky/settings.py
+++ btracky/settings.py
@@ -133,12 +133,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
