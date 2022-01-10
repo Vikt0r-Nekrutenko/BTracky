d0bb3fc86f9056601f256524b7f097d0520d3df7
Vikr0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2022-01-10 17:55:38 +0200

    094

--- btracky/settings.py
+++ btracky/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-am9_(y^lq&o!*&o^qq4c2+_+4e=jk3f=%*c)d60^*$ldvtsk#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'btracky.herokuapp.com',
@@ -124,7 +124,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
@@ -144,7 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+CSRF_COOKIE_SECURE = True
 
 django_heroku.settings(locals())
 
